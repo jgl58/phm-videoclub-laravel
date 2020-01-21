d9f7ce8773428018194165826dc0b8154833eb24
Jonay Gilabert López <jgl58@alu.ua.es>
2020-01-21 19:35:51 +0100

    Ejercicios 3 hechos

--- resources/views/catalog/show.blade.php
+++ resources/views/catalog/show.blade.php
@@ -5,26 +5,26 @@
 <div class="row">
 
     <div class="col-sm-4">
-        <img src="{{$pelicula['poster']}}" style="height:400px"/>
+        <img src="{{$pelicula->poster}}" style="height:400px"/>
     </div>
     <div class="col-sm-8">
-        <h1>{{$pelicula['title']}}</h1>
-        <h4>Año: {{$pelicula['year']}}</h4>
-        <h4>Director: {{$pelicula['director']}}</h4>
-        <p><strong>Resumen: </strong>{{$pelicula['synopsis']}}</p>
+        <h1>{{$pelicula->title}}</h1>
+        <h4>Año: {{$pelicula->year}}</h4>
+        <h4>Director: {{$pelicula->director}}</h4>
+        <p><strong>Resumen: </strong>{{$pelicula->synopsis}}</p>
         <p><strong>Estado: </strong>
-            @if($pelicula['rented'])
+            @if($pelicula->rented)
                 Película actualmente alquilada
             @else
                 Película no alquilada
             @endif
         </p>
-        @if($pelicula['rented'])
+        @if($pelicula->rented)
             <button type="button" class="btn btn-danger">Devolver la película</button>
         @else
             <button type="button" class="btn btn-primary">Alquilar película</button>
         @endif
-        <a type="button" class="btn btn-warning">Editar película</a>
+        <a type="button" class="btn btn-warning" href={{'/catalog/edit/'.$pelicula->id}}>Editar película</a>
         <button type="button" class="btn btn-light">Volver al listado</button>
     </div>
 </div>
